727384222ace77197d522f8861b9dbd39c077864
Caio99BR <caiooliveirafarias0@gmail.com>
2015-01-26 16:36:21 -0200

    msm: board7627a: Support ZSL with 512MB RAM when CMA is enabled
    To support advanced camera features like ZSL with 5MP
    sensor, we require camera_preview heap to be of 60 MB.
    Allocate this size with 512MB RAM only when we enable
    CMA.
    
    Change-Id: Id4ec9e561a3d5eb2f6cc1cc4ad59fa83d3857bb9
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- arch/arm/mach-msm/lge/vee3/board-msm7x27a_vee3.c
+++ arch/arm/mach-msm/lge/vee3/board-msm7x27a_vee3.c
@@ -731,12 +731,13 @@ static void fix_sizes(void)
 	if (get_ddr_size() > SZ_512M)
 		reserve_adsp_size = CAMERA_ZSL_SIZE;
 #ifdef CONFIG_ION_MSM
-	msm_ion_camera_size = reserve_adsp_size;
 	msm_ion_audio_size = MSM_RESERVE_AUDIO_SIZE;
 	msm_ion_sf_size = reserve_mdp_size;
 #ifdef CONFIG_CMA
+	msm_ion_camera_size = CAMERA_ZSL_SIZE;
 	msm_ion_camera_size_carving = 0;
 #else
+	msm_ion_camera_size = reserve_adsp_size;
 	msm_ion_camera_size_carving = msm_ion_camera_size;
 #endif
 #endif
